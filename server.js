cfa47d81a752a11f2b02107b4b321d6392e35a1f
Govind Kumar <137143670+govindjee725@users.noreply.github.com>
2025-12-17 19:36:19 +0530

    Change server port from 5000 to 3000

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use("/api/family", familyRoutes);
 
 app.use("/api/medicines", medicineRoutes);
 app.use("/api/medicine-logs", medicineLogRoutes);
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.get("/",(req,res)=>{
   res.send("hello");
 })
